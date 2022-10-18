02236e71b6958eb228811cf1a11a7c04ac273f86
jrfelliplourde <31169498+jrfelliplourde@users.noreply.github.com>
2022-10-18 12:16:24 -0400

    Update ft_strcpy.c

--- PiscineSept2022/C/C02/ex00/ft_strcpy.c
+++ PiscineSept2022/C/C02/ex00/ft_strcpy.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fjean-ra <fjean-ra@student.42quebec.com    +#+  +:+       +#+        */
+/*   By: fjean-ra <fjean-ra@student.42quebec.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 11:56:35 by fjean-ra          #+#    #+#             */
 /*   Updated: 2022/09/27 14:57:15 by fjean-ra         ###   ########.fr       */
